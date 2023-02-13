e3d5becc42bcebee6aaeedea58e715532cd14aa2
Daniel <danielgillies2005@gmail.com>
2023-02-13 19:55:45 +0000

    Better prefixes and cleaned up code

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-require("dotenv").config();
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { Configuration, OpenAIApi } = require("openai");
+import dotenv from "dotenv";
+import { Client, Events, GatewayIntentBits, Collection } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 
-const PREFIX = ".";
+dotenv.config();
+
+const PREFIX = "K-9 ";
+const PREFIX2 = ",";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -19,45 +24,105 @@ const client = new Client({
   ],
 });
 
+let channel = "1018199943774732410";
+let lastChannel = channel;
+let c;
+
+async function reader() {
+  const rl = readline.createInterface({ input, output });
+
+  c = client.channels.cache.get(channel);
+  console.log(`#${c.name}`);
+
+  while (true) {
+    const answer = await rl.question("~> ");
+    if (answer.indexOf("channel") === 0) {
+      lastChannel = channel;
+      channel = answer.split(" ")[1];
+      c = client.channels.cache.get(channel);
+
+      if (!c) {
+        channel = lastChannel;
+        c = client.channels.cache.get(channel);
+        console.log("\x1b[31minvalid channel id\x1b[0m");
+        console.log(`\n#${c.name}`);
+      } else {
+        console.log(`\n#${c.name}`);
+      }
+    } else if (answer.trim().length) {
+      client.channels.cache.get(channel).send(answer);
+
+      //dm a user
+
+      // client.users.fetch("394153008054796299", false).then((user) => {
+      //   user.send(answer);
+      // });
+    }
+  }
+}
+
+function safeReply(message, reply) {
+  message
+    .reply(reply)
+    .catch(() => message.channel.send(`<@${message.author.id}> ${reply}`));
+}
+async function getGptResponse(prompt, model) {
+  const gptResponse = await openai.createCompletion({
+    model: model,
+    prompt: `${prompt}. ###`,
+    max_tokens: 60,
+    temperature: 0.3,
+    top_p: 0.3,
+    presence_penalty: 0,
+    frequency_penalty: 0.5,
+    stop: ["\n", "END"],
+  });
+  const reply = `${gptResponse.data.choices[0].text.trim()}`;
+  if (reply.length) {
+    return reply;
+  } else {
+    return "Input unknown. Please try again. (This is an error, not an AI response)";
+  }
+}
+
 client.on("ready", () => {
   console.log("Logged in as " + client.user.username);
+  reader();
 });
 
 client.on("messageCreate", function (message) {
   if (
-    // message.author === client.user ||
-    !(message.author.id === "1037466389163814932") ||
-    message.content.indexOf(PREFIX) === 0
-  )
+    message.author.bot ||
+    !(
+      message.content.indexOf(PREFIX) === 0 ||
+      (message.content.indexOf(PREFIX2) === 0 &&
+        message.author.id === process.env.PRIVV)
+    )
+  ) {
     return;
-
-  openai.createModeration({ input: message.content }).then((res) => {
+  }
+  openai.createModeration({ input: message.content }).then(async (res) => {
     if (res.data.results[0].flagged) {
-      message.reply(
-        "this is not a AI generated message, it is moderated, don't be a bitch and get me banned, ty and fuck you"
+      safeReply(
+        message,
+        "Your message has been moderated. Please refrain from trying to generate the following content: hate, self-harm, sexual, violence. (This is an error, not an AI response)"
       );
+    } else if (message.content.indexOf(PREFIX) === 0) {
+      const gptResponse = await getGptResponse(
+        message.content.substring(3),
+        "ada:ft-personal:k-9-mark-ii-1-2023-02-11-19-23-19",
+        console.log(message.content.substring(3))
+      );
+      safeReply(message, gptResponse);
     } else {
-      (async () => {
-        const gptResponse = await openai.createCompletion({
-          model: "davinci:ft-personal:k-9-2023-02-10-21-04-50",
-          prompt: `${message.content}. ###`,
-          max_tokens: 60,
-          temperature: 0.3,
-          top_p: 0.3,
-          presence_penalty: 0,
-          frequency_penalty: 0.5,
-          stop: ["\n", "END"],
-        });
-        const reply = `${gptResponse.data.choices[0].text.trim()}`;
-        if (reply.length) {
-          message.reply(reply);
-        } else {
-          message.reply("Sorry, there was an error, try again.");
-        }
-      })();
+      const gptResponse = await getGptResponse(
+        message.content.substring(1),
+        "davinci:ft-personal:k-9-mark-ii-2023-02-11-16-43-01"
+      );
+      safeReply(message, gptResponse);
     }
+    return;
   });
-  return;
 });
 
 client.login(process.env.TOKEN);
