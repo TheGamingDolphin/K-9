89f9af7deccaa2f10f2e18a1a8fbbe79e6d59cf3
Daniel <danielgillies2005@gmail.com>
2023-03-08 16:12:11 +0000

    Added slash command

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import dotenv from "dotenv";
-import { Client, Events, GatewayIntentBits, Collection } from "discord.js";
-import { Configuration, OpenAIApi } from "openai";
-import * as readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
-import express from "express";
-import fs from "fs";
+const dotenv = require("dotenv");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+const readline = require("node:readline/promises");
+const { stdin: input, stdout: output } = require("node:process");
+const express = require("express");
+const fs = require("node:fs");
+const path = require("node:path");
 
 dotenv.config();
 
@@ -221,5 +222,58 @@ app.get("/K-9_4", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
+
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
 
 client.login(process.env.TOKEN);
