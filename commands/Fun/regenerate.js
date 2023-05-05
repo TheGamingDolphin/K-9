9acf422b06e73dfd102de6564c94b429ad4fbeb1
Daniel <danielgillies2005@gmail.com>
2023-05-05 17:00:54 +0100

    made current nickname stay along with the /regenerate command

--- commands/Fun/regenerate.js
+++ commands/Fun/regenerate.js
@@ -5,9 +5,9 @@ module.exports = {
     .setName("regenerate")
     .setDescription("Love from Gallifrey, boys!"),
   async execute(interaction) {
+    const member = interaction.member.user.username;
     let canNickname = true;
     const randomNumber = Math.floor(Math.random() * 17) + 1;
-    console.log(randomNumber);
     if (
       !interaction.channel
         .permissionsFor(interaction.client.user)
@@ -23,49 +23,145 @@ module.exports = {
     if (randomNumber === 1) {
       await interaction.reply("https://i.gifer.com/Ac0v.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The 2nd Doctor");
+        const nicknameWithUsername = `The 2nd Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 2nd Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 2) {
       //2 to 3
       await interaction.reply("https://i.gifer.com/EHul.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The 3rd Doctor");
+        const nicknameWithUsername = `The 3rd Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 3rd Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 3) {
       //3 to 4
       await interaction.reply("https://i.gifer.com/Gx6Y.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The 4th Doctor");
+        const nicknameWithUsername = `The 4th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 4th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 4) {
       //4 to 5
       await interaction.reply("https://i.gifer.com/EHun.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The 5th Doctor");
+        const nicknameWithUsername = `The 5th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 5th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 5) {
       //5 to 6
       await interaction.reply("https://i.gifer.com/EHum.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The 6th Doctor");
+        const nicknameWithUsername = `The 6th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 6th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 6) {
       //6 to 7
       await interaction.reply("https://i.gifer.com/EHuq.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The 7th Doctor");
+        const nicknameWithUsername = `The 7th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 7th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 7) {
       //7 to 8
       await interaction.reply("https://i.gifer.com/3KMn.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The 8th Doctor");
+        const nicknameWithUsername = `The 8th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 8th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 8) {
       //8 to War
       await interaction.reply("https://i.gifer.com/Gx6k.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The War Doctor");
+        const nicknameWithUsername = `The War Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The War Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 9) {
       //War to 9
@@ -73,13 +169,37 @@ module.exports = {
         "https://thumbs.gfycat.com/ImpoliteYellowCaterpillar-max-1mb.gif"
       );
       if (canNickname === true) {
-        interaction.member.setNickname("The 9th Doctor");
+        const nicknameWithUsername = `The 9th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 9th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 10) {
       //9 to 10
       await interaction.reply("https://tenor.com/view/-gif-3538028");
       if (canNickname === true) {
-        interaction.member.setNickname("The 10th Doctor");
+        const nicknameWithUsername = `The 10th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 10th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 11) {
       //10 to 10
@@ -87,7 +207,19 @@ module.exports = {
         "https://media2.giphy.com/media/CoKJD9a9pxc9W/giphy.gif"
       );
       if (canNickname === true) {
-        interaction.member.setNickname("The 10th Doctor");
+        const nicknameWithUsername = `The 10th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 10th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 12) {
       //10 to 11
@@ -95,13 +227,37 @@ module.exports = {
         "https://media.tenor.com/WH5GS4DSS78AAAAd/doctor-who-david-tennant.gif"
       );
       if (canNickname === true) {
-        interaction.member.setNickname("The 11th Doctor");
+        const nicknameWithUsername = `The 11th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 11th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 13) {
       //11 to 12
       await interaction.reply("https://i.gifer.com/EHuf.gif");
       if (canNickname === true) {
-        interaction.member.setNickname("The 12th Doctor");
+        const nicknameWithUsername = `The 12th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 12th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 14) {
       //12 to 13
@@ -109,7 +265,19 @@ module.exports = {
         "https://64.media.tumblr.com/af05ce9b690536924d0216893012bb3b/tumblr_p1v23yqP9U1tgl57yo1_540.gif"
       );
       if (canNickname === true) {
-        interaction.member.setNickname("The 13th Doctor");
+        const nicknameWithUsername = `The 13th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 13th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 15) {
       //13 to Master
@@ -117,7 +285,19 @@ module.exports = {
         "https://media.tenor.com/7GPggFxYxBQAAAAd/regenoration-13th-doctor.gif"
       );
       if (canNickname === true) {
-        interaction.member.setNickname("The Master Doctor");
+        const nicknameWithUsername = `The Master Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The Master Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 16) {
       //Master to 13
@@ -125,7 +305,19 @@ module.exports = {
         "https://64.media.tumblr.com/d97f3c6fe15a54747e1c78ebf28ae774/6738b19fb6d790ad-5e/s540x810/e5ee3bb300c9bcccd2e24b6d16dd1c91d74d44e4.gif"
       );
       if (canNickname === true) {
-        interaction.member.setNickname("The 13th Doctor");
+        const nicknameWithUsername = `The 13th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 13th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     } else if (randomNumber === 17) {
       //13 to 14
@@ -133,7 +325,19 @@ module.exports = {
         "https://media.tenor.com/4mOyPi1ZQNoAAAAd/doctor-doctor-who.gif"
       );
       if (canNickname === true) {
-        interaction.member.setNickname("The 14th Doctor");
+        const nicknameWithUsername = `The 14th Doctor (${interaction.member.displayName})`;
+
+        if (nicknameWithUsername.length <= 32) {
+          interaction.member
+            .setNickname(nicknameWithUsername)
+            .catch((error) => {
+              console.error("Error setting nickname with username:", error);
+            });
+        } else {
+          interaction.member.setNickname("The 14th Doctor").catch((error) => {
+            console.error("Error setting nickname without username:", error);
+          });
+        }
       }
     }
   },
