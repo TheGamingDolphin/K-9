d5557b57ad17470c09da3c9beee5fc5011ac42c2
Daniel <danielgillies2005@gmail.com>
2024-04-28 15:00:50 +0100

    fixed countdown

--- index.js
+++ index.js
@@ -163,14 +163,10 @@ client.on("ready", async () => {
     if (now.getHours() === 0 && now.getMinutes() === 0) {
       try {
         const channel = client.channels.cache.get("1018199943774732410");
-        channel.send(
-          `Don't forget to click below to subscribe to the official Doctor Who YouTube channel\nhttp://bit.ly/SubscribeToDoctorWho`
-        );
+        channel.send(`Series 14 releases <t:1715382000:R>!`);
       } catch {
         const channel = client.channels.cache.get("915568009815416845");
-        channel.send(
-          `Don't forget to click below to subscribe to the official Doctor Who YouTube channel\nhttp://bit.ly/SubscribeToDoctorWho`
-        );
+        channel.send(`Series 14 releases <t:1715382000:R>!`);
       }
     }
     // scheduled restart
@@ -186,10 +182,10 @@ client.on("ready", async () => {
     if (now.getHours() === 12 && now.getMinutes() === 0) {
       try {
         const channel = client.channels.cache.get("1018199943774732410");
-        channel.send(`Series 14 releases <t:1715295600:R>!`);
+        channel.send(`Series 14 releases <t:1715382000:R>!`);
       } catch {
         const channel = client.channels.cache.get("915568009815416845");
-        channel.send(`Series 14 releases <t:1715295600:R>!`);
+        channel.send(`Series 14 releases <t:1715382000:R>!`);
       }
     }
     // scheduled restart
